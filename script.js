b3de6d50da08141df6d2f22603477a9671ef7e3c
Nyi Zin Thant <codebynyi@gmail.com>
2022-07-05 13:23:40 +0630

    store selected dom

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const domModal = document.querySelector('.modal');
+const domOverlay = document.querySelector('.overlay');
+const domClose = document.querySelector('.close-modal');
+const domModalBtns = document.querySelectorAll('.show-modal');
