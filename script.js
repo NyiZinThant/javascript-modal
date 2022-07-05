8a9a8a5418d62fa51c7c299448c28f9c6ab8aec6
Nyi Zin Thant <codebynyi@gmail.com>
2022-07-05 13:40:18 +0630

    open and close modal feature

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const domModal = document.querySelector('.modal');
 const domOverlay = document.querySelector('.overlay');
 const domClose = document.querySelector('.close-modal');
 const domModalBtns = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  domModal.classList.remove('hidden');
+  domOverlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  domModal.classList.add('hidden');
+  domOverlay.classList.add('hidden');
+  console.log('click');
+};
+
+for (let i = 0; i < domModalBtns.length; i++)
+  domModalBtns[i].addEventListener('click', openModal);
+
+domClose.addEventListener('click', closeModal);
+domOverlay.addEventListener('click', closeModal);
